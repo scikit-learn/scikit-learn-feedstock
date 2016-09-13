68d2e88d799a998be238838bada2528ed6868001
Andreas Mueller <amueller@nyu.edu>
2016-09-13 11:19:25 -0400

    trying to update to build 0.17b1 for testing

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -82,5 +82,5 @@ source run_conda_forge_build_setup
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root scikit-learn --channel=rc || exit 1
 EOF
